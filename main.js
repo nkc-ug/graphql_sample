fd1612b2b078f94d550b73c1c53158202d2f50ac
Hisada <hissa@users.noreply.github.com>
2019-07-23 23:25:58 +0900

    Add comments.

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const expressGraphql = require('express-graphql');
 const { buildSchema } = require('graphql');
-const books = require('./data');
+const books = require('./data'); // 元データ
 
 const fs = require('fs');
 const schemaText = fs.readFileSync('./schema.graphql', {encoding: 'utf-8'});
-const schema = buildSchema(schemaText);
+const schema = buildSchema(schemaText); // schema.graphqlから文字列としてbuildSchemaに通す
 
+// 1件の本のデータを取得
 const getBook = args => {
     const id = args.id;
     return books.find(b => b.id === id);
 };
 
+// 本のデータの一覧を取得
 const getBooks = args => {
     const category = args.category;
     if (category === undefined) {
@@ -20,6 +22,7 @@ const getBooks = args => {
     return books.filter(b => b.category === category);
 };
 
+// 本のデータを追加
 const addBook = args => {
     const title = args.title;
     const author = args.author;
@@ -36,23 +39,29 @@ const addBook = args => {
     return books.find(b => b.id === id);
 };
 
+// "ルート"を定義
 const root = {
     book: getBook,
     books: getBooks,
     addBook: addBook
 };
 
+// サーバーを作成
 const app = express();
+
+// GUI付きで待受け
 app.use('/graphql', expressGraphql({
     schema: schema,
     rootValue: root,
     graphiql: true
 }));
 
+// GUIなしで待受け
 app.use('/query', expressGraphql({
     schema: schema,
     rootValue: root,
     graphiql: false
 }));
 
+// サーバー起動
 app.listen(3000, () => console.log('Running...'));
